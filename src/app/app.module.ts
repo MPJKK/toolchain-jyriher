fc645cee7157000841cb7546ab34dc3b6b0beff5
Jyri Herka <jyriher@ety-a2123b-17.edu.wks.metropolia.fi>
2018-01-16 13:05:56 +0200

    ensimmäinen tehtävä

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { SiderbarComponent } from './siderbar/siderbar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ContainerBoxComponent } from './container-box/container-box.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    SiderbarComponent,
+    MainContentComponent,
+    ContainerBoxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/container-box/container-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container-box',
+  templateUrl: './container-box.component.html',
+  styleUrls: ['./container-box.component.scss']
+})
+export class ContainerBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
